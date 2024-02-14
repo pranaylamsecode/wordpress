b46ed2348cabc4c66d465b725d1a19593d5edf5e
pranaylamsecode <pranaylamsecode@gmail.com>
2024-02-14 16:39:16 +0530

    init

---
+++ wp-content/themes/pranay-site/function.php
@@ -0,0 +1,7 @@
+<?php
+function add_css()
+{
+   wp_register_style('first', get_template_directory_uri() . '/assets/css/style-starter.css', false,'1.1','all');
+   wp_enqueue_style( 'first');
+}
+add_action('wp_enqueue_scripts', 'add_css');
